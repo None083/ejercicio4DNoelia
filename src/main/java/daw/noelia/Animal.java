17111dcd81dbfd2ed63ff74bf3ba47af8bdcffcd
Noelia <perezgonzaleznoelia08@gmail.com>
2022-11-23 14:11:17 +0100

    231122 1411

--- src/main/java/daw/noelia/Animal.java
+++ src/main/java/daw/noelia/Animal.java
@@ -9,7 +9,70 @@ package daw.noelia;
  * @author noelia
  */
 public class Animal {
-    String fechaNacimiento;
-    String nombre;
+    private String fechaNacimiento;
+    private String nombre;
+    private String tipo;
+    private int peso;
+    private String estado;
+
+    public Animal() {
+    }
+
+    public Animal(String fechaNacimiento, String nombre, String tipo, int peso, String estado) {
+        this.fechaNacimiento = fechaNacimiento;
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.peso = peso;
+        this.estado = estado;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" + "fechaNacimiento=" + fechaNacimiento + ", nombre=" + nombre + ", tipo=" + tipo + ", peso=" + peso + ", estado=" + estado + '}';
+    }
+    
+    public static void comer(double cantidadGramos){
+        
+    }
     
 }
