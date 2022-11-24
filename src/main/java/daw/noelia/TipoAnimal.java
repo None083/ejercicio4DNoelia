8caf0176312fc604cd5b4ddce33496541fd6d0fa
Noelia <perezgonzaleznoelia08@gmail.com>
2022-11-24 12:48:38 +0100

    241122

---
+++ src/main/java/daw/noelia/TipoAnimal.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package daw.noelia;
+
+/**
+ *
+ * @author noelia
+ */
+public enum TipoAnimal {
+    
+    
+    
+    private int codigoTipo;
+    private String tipo;
+    
+}
